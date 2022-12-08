e7f2867cfcb12d9ac29f9315da5f3734adf3feec
bhavin-02 <bhaving1112@gmail.com>
2022-12-08 15:00:47 +0530

    volly

--- app/src/main/java/com/mobearn/ad/ui/home/HomeFragment.kt
+++ app/src/main/java/com/mobearn/ad/ui/home/HomeFragment.kt
@@ -1,14 +1,29 @@
 package com.mobearn.ad.ui.home
 
+import android.app.ProgressDialog.show
 import android.os.Bundle
+import android.util.Log
 import android.view.*
 import android.view.MotionEvent.*
 import androidx.fragment.app.Fragment
 import android.webkit.WebViewClient
+import android.widget.ImageView
+import android.widget.ProgressBar
+import android.widget.TextView
+import android.widget.Toast
 import androidx.core.view.isGone
 import androidx.core.view.isInvisible
+import com.android.volley.Request
+import com.android.volley.RequestQueue
+import com.android.volley.Response
+import com.android.volley.toolbox.JsonArrayRequest
+import com.android.volley.toolbox.JsonObjectRequest
+import com.android.volley.toolbox.Volley
 import com.mobearn.ad.R
+import com.squareup.picasso.Picasso
 import kotlinx.android.synthetic.main.fragment_home.*
+import org.json.JSONArray
+import org.json.JSONException
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -20,6 +35,11 @@ import kotlinx.android.synthetic.main.fragment_home.*
  */
 class HomeFragment : Fragment() {
     // TODO: Rename and change types of parameters
+    lateinit var courseIV: ImageView
+    lateinit var courseNameTV: TextView
+    lateinit var loadingPB: ProgressBar
+  //  private var requestQueue: RequestQueue? = null
+    var url = "https://mobearn.000webhostapp.com/admin/movie.php"
 
 
     override fun onCreateView(
@@ -57,8 +77,63 @@ class HomeFragment : Fragment() {
         }
         movie.setOnClickListener {
             layout.visibility=View.VISIBLE
+
         }
-        webview.setOnKeyListener(object : View.OnKeyListener {
+
+
+
+
+
+       // var url = "https://mobearn.000webhostapp.com/admin/movie.php"
+        val queue: RequestQueue = Volley.newRequestQueue(context)
+
+        courseNameTV = urltv
+        courseIV = idIVCourse
+
+        val request = JsonArrayRequest(Request.Method.GET, url, null, { response ->
+
+            // this method is called when we get a successful response from API.
+            // we are setting the visibility of progress bar as gone.
+            // on below line we are adding a try catch block.
+           // loadingPB.setVisibility(View.GONE)
+            loadingPB = idLoadingPB
+            loadingPB.visibility = View.GONE
+            try {
+                // on below line we are getting data from our response
+                // and setting it in variables.
+
+                val jsonobject = response.getJSONObject(1)
+                val name = jsonobject.getString("Moviename")
+                val image = jsonobject.getString("Poster")
+               // val courseName: String = response.getString("Banner")
+                courseNameTV.text= name
+                // on below line we are setting
+                // image view from image url.
+                Picasso.get().load(image).into(courseIV)
+                // on below line we are changing
+                // visibility for our button.
+              //  visitCourseBtn.visibility = View.VISIBLE
+                // on below line we are adding
+                // click listener for our button.
+            } catch (e: Exception) {
+                // on below line we are
+                // handling our exception.
+                e.printStackTrace()
+            }
+        }, { error ->
+            // this method is called when we get
+            // any error while fetching data from our API
+            Log.e("TAG", "RESPONSE IS $error")
+            // in this case we are simply displaying a toast message.
+            Toast.makeText(activity, "Fail to get response", Toast.LENGTH_SHORT).show()
+
+        })
+        // at last we are adding
+        // our request to our queue.
+        queue.add(request)
+
+
+       /* webview.setOnKeyListener(object : View.OnKeyListener {
             override fun onKey(v: View?, keyCode: Int, event: KeyEvent): Boolean {
                 if ((keyCode == KeyEvent.KEYCODE_BACK) && event.action === ACTION_UP && webview.canGoBack()) {
                     //  handler.sendEmptyMessage(1)
@@ -67,9 +142,38 @@ class HomeFragment : Fragment() {
                 return false
             }
 
-        })
+        })*/
 
 
 
 }
-}
+    /*private fun jsonParse() {
+        val url = "https://mobearn.000webhostapp.com/admin/movie.php"
+        val request = JsonObjectRequest(Request.Method.GET, url, null, {
+                response ->try {
+            val jsonArray = response.getJSONArray("")
+            for (i in 0 until jsonArray.length()) {
+                val employee = jsonArray.getJSONObject(i)
+                val firstName = employee.getString("Banner")
+                urltv.text = firstName
+
+
+            }
+        } catch (e: JSONException) {
+            e.printStackTrace()
+        }
+        },Response.ErrorListener { error -> error.printStackTrace() })
+        requestQueue?.add(request)*/
+
+    }
+
+
+
+
+
+
+
+
+
+
+
